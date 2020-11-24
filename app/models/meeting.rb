e9737a65b97930e9f2ee3bc6d30b1493409b5b69
Nicholas Marques <nnmarques97@gmail.com>
2020-11-23 23:08:59 -0300

    ✅ adicionadas validacoes e testes para meetings

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,14 @@
 class Meeting < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  validates :title, :meeting_end, :meeting_start, presence: true
+  validate :end_date_is_after_start_date
+
+  private
+
+  def end_date_is_after_start_date
+    return if meeting_end.blank? || meeting_start.blank?
+
+    errors.add(:meeting_end, 'cannot be before the meeting end') if meeting_end < meeting_start
+  end
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Meeting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'model validations' do
+    it 'is valid with valid attributes' do
+      meeting = build(:meeting)
+      expect(meeting).to be_valid
+    end
+
+    it 'is not valid without a title' do
+      meeting = build(:meeting, title: nil)
+      expect(meeting).to_not be_valid
+    end
+
+    it 'is not valid without a start date' do
+      meeting = build(:meeting, meeting_start: nil)
+      expect(meeting).to_not be_valid
+    end
+
+    it 'is not valid without a end date' do
+      meeting = build(:meeting, meeting_end: nil)
+      expect(meeting).to_not be_valid
+    end
+
+    it 'is not valid with a start date greater then end date' do
+      date_aux = DateTime.now
+      meeting = build(:meeting, meeting_start: date_aux + 1, meeting_end: date_aux)
+      expect(meeting).to_not be_valid
+    end
+  end
 end
