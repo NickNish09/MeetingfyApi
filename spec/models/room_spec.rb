fbf4e905f1155a17d5b62da030d202f509064e3d
Nicholas Marques <nnmarques97@gmail.com>
2020-11-24 10:50:58 -0300

    ✨ atualizada funcao de room available

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -13,7 +13,37 @@ RSpec.describe Room, type: :model do
 
       it 'returns false' do
         # advancing meeting_start in 1 hour to be between the meeting schedule
+        expect(@room.available?(@date_aux.advance(hours: 1), @date_aux.advance(hours: 4))).to be_falsey
+      end
+    end
+
+    context "when the meeting_end is between other meeting schedule" do
+      before do
+        @date_aux = DateTime.now
+        @room = create(:room)
+        # creating a date with 3 hours range
+        @meeting = create(:meeting, room: @room,
+                          meeting_start: @date_aux, meeting_end: @date_aux.advance(hours: 3))
+      end
+
+      it 'returns false' do
+        # advancing meeting_start in 1 hour to be between the meeting schedule
         expect(@room.available?(@date_aux.advance(hours: 1), @date_aux.advance(hours: 2))).to be_falsey
+      end
+    end
+
+    context "when start and end are not between other meeting schedule" do
+      before do
+        @date_aux = DateTime.now
+        @room = create(:room)
+        # creating a date with 3 hours range
+        @meeting = create(:meeting, room: @room,
+                          meeting_start: @date_aux, meeting_end: @date_aux.advance(hours: 3))
+      end
+
+      it 'returns true' do
+        # advancing meeting_start in 1 hour to be between the meeting schedule
+        expect(@room.available?(@date_aux.advance(hours: 4), @date_aux.advance(hours: 6))).to be_truthy
       end
     end
   end
